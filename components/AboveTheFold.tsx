8fc373479541753c950c2727fd0076af2f7cea3a
lelelew <lele@lelelew.com>
2023-02-28 20:58:23 -0800

    update resume

--- components/AboveTheFold.tsx
+++ components/AboveTheFold.tsx
@@ -10,12 +10,15 @@ export function AboveTheFold() {
         <h1 className={classes.name}>Lele Lew</h1>
         <h2 className={classes.title}>Web Developer</h2>
         <p className={classes.description}>
-          I'm a self-taught web developer with a background in startups and financial auditing.  I like to research, understand, and build. My passion for development comes from seeing things <strong>work</strong>.
+          I'm a self-taught web developer with a background in startups and
+          financial auditing. I like to research, understand, and build. My
+          passion for development comes from seeing things <strong>work</strong>
+          .
         </p>
         <div className={classes.buttons}>
           <a href="mailto:lele@lelelew.com">Contact</a>
           <a href="https://github.com/lelelew">Github</a>
-          <a href="/Lele%20Lew%20-%20Resume%20-%20November%202020.pdf">Resume</a>
+          <a href="/Lele%20Lew%20-%20Resume.pdf">Resume</a>
         </div>
         <small className={classes.label}>
           Works with (and am looking to learn more)
